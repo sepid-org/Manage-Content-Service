096dfcdc111d502de0ef974062db16b344f81108
Amir Hossein <54184618+amirhh-2000@users.noreply.github.com>
2025-04-27 01:31:10 +0330

    Add generic handling for all not_found exceptions

--- content_management_service/exception_handler.py
+++ content_management_service/exception_handler.py
@@ -4,7 +4,7 @@ from rest_framework.views import exception_handler
 
 from apps.core.exceptions import ServiceException
 
-
+# TODO: make this function more generic
 def custom_exception_handler(exc, context):
     # Call DRF's default exception handler first
     response = exception_handler(exc, context)
@@ -28,8 +28,7 @@ def custom_exception_handler(exc, context):
 
         status_code = status.HTTP_400_BAD_REQUEST
 
-        # Map specific exceptions to specific status codes
-        if exc.code == "discount_code_not_found":
+        if "_not_found" in exc.code:
             status_code = status.HTTP_404_NOT_FOUND
 
         return Response(data, status=status_code)
